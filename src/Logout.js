747a78ae5a6b6c22f4a98621670950674955dfaa
Mohamed <mohamedelshazly18@yahoo.com>
2021-05-02 00:18:42 +0200

    trying to fix logout issue

--- src/Logout.js
+++ src/Logout.js
@@ -3,14 +3,14 @@ import { Link, useHistory } from "react-router-dom";
 import Axios from 'axios'
 
 export default function Logout() {
-    // const history = useHistory()
+    const history = useHistory()
     Axios({
         method:"GET", 
         withCredentials:true,
         url:"https://new-habits.herokuapp.com/auth/logout"
     }).then(() =>{
         window.location.reload(false);
-        // history.push('/login')
+        history.push('/login')
     })
     return (
         <div>
